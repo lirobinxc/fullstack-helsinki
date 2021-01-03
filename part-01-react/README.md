06c364d02cb15a81b426e50cddae406607a699a2
Robin Li <lirobinxc@gmail.com>
2021-01-02 21:47:56 -0500

    added solution

---
+++ part-01-react/README.md
@@ -0,0 +1,18 @@
+# Full Stack Web Development @ U of Helsinki
+
+## Part 1
+
+### Topics studied
+
+- Component state, event handlers
+- Debugging React apps
+- Complex state
+- Handling arrays
+- Conditional rendering
+- Functional vs older Class Components
+- Debugging React applications
+- Rules of Hooks
+- Event Handling Revisited
+- Function that returns a function
+- Passing Event Handlers to Child Components
+- Do Not Define Components Within Components
---
+++ part-01-react/src/App.js
@@ -0,0 +1,22 @@
+import {useState} from 'react';
+import Button from './components/Button'
+
+export default function App() {
+  const [feedbackGood, setFeedbackGood] = useState(0);
+  const [feedbackNeutral, setFeedbackNeutral] = useState(0);
+  const [feedbackBad, setFeedbackBad] = useState(0);
+
+
+  return (
+    <div>
+      <h1>Give Feedback</h1>
+      <Button onClick={() => setFeedbackGood(feedbackGood + 1)} text="Good" />
+      <Button onClick={() => setFeedbackNeutral(feedbackNeutral + 1)} text="Neutral" />
+      <Button onClick={() => setFeedbackBad(feedbackBad + 1)} text="Bad" />
+      <h1>Statistics</h1> 
+      <p>Good: {feedbackGood}</p>
+      <p>Neutral: {feedbackNeutral}</p>
+      <p>Bad: {feedbackBad}</p>
+    </div>
+  )
+}
