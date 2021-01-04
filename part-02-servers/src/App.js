42f5b50a052a652e545c7630e4dd746a7eed46d0
Robin Li <lirobinxc@gmail.com>
2021-01-03 19:15:53 -0500

    phonebook updated

--- part-02-servers/src/App.js
+++ part-02-servers/src/App.js
@@ -1,74 +1,13 @@
-import { useState } from 'react';
-import Filter from './components/Filter'
+import axios from 'axios'
 
-const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', phone: '040-123456' },
-    { name: 'Ada Lovelace', phone: '39-44-5323523' },
-    { name: 'Dan Abramov', phone: '12-43-234345' },
-    { name: 'Mary Poppendieck', phone: '39-23-6423122' }
-  ])
-  const [ newName, setNewName ] = useState('')
-  const [ newPhone, setNewPhone ] = useState('')
+export default function App() {
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    const newObj = {
-      name: newName,
-      phone: newPhone
-    }
-
-    const namesArr = persons.map((ele, i) => {
-      return ele.name
-    })
-    const phoneArr = persons.map((ele, i) => {
-      return ele.phone
+  axios.get('http://localhost:3005/notes')
+    .then(res => {
+      const notes = res.data
+      console.log('📣 notes ~', notes)
     })
 
+  return "Welcome"
 
-    if (namesArr.includes(newName)) {
-      alert(`"${newName}" already exists.`);
-      return;
-    }
-    if (phoneArr.includes(newPhone)) {
-      alert(`"${newPhone}" already exists.`);
-      return;
-    }
-
-    setPersons(persons.concat(newObj))
-    setNewName('')
-  }
-
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
-  const handlePhoneChange = (event) => {
-    setNewPhone(event.target.value)
-  }
-
-  return (
-    <div>
-      <h2>Phonebook</h2>
-      <Filter persons={persons} />
-      <hr />
-      <form onSubmit={handleSubmit}>
-        <div>
-          name: <input value={newName} onChange={handleNameChange} required/>
-        </div>
-        <div>
-          phone: <input value={newPhone} onChange={handlePhoneChange} required/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      
-      
-      <div>debug: {newName}</div>
-    </div>
-    
-  )
 }
-
-export default App
