15cad8a1f94f7613a4d6232f5a54e32d054af671
lirobinxc <lirobinxc@gmail.com>
2021-01-20 20:12:11 -0500

    minor changes

---
+++ part-02-servers/test.sql
@@ -0,0 +1,12 @@
+CREATE TABLE cities (
+  name VARCHAR(50),
+  country VARCHAR(50),
+  population INTEGER,
+  area INTEGER
+);
+
+INSERT INTO cities (name, country, population, area)
+	VALUES
+  	('Delhi', 'India', 28124500, 2248)
+    ('Shanghai', 'China', 23193000, 4015)
+    ('Toyko', 'Japan', 218939920, 1029);
