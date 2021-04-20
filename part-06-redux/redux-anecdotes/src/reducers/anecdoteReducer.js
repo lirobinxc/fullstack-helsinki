079d3e33d8fc07f22728d394c9b1e8a289dca442
lirobinxc <lirobinxc@gmail.com>
2021-04-20 01:31:24 -0400

    changed intitalState

--- part-06-redux/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ part-06-redux/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -21,7 +21,7 @@ const asObject = (anecdote) => {
 
 const initialState = anecdotesAtStart.map(asObject);
 
-const anecdoteReducer = (state = [], action) => {
+const anecdoteReducer = (state = initialState, action) => {
   console.log('state now: ', state);
   console.log('action', action);
   switch (action.type) {
